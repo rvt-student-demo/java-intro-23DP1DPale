b2280589ca1f675516bc321125cea287d24cc657
23DP1DPale <166118206+23DP1DPale@users.noreply.github.com>
2024-09-24 07:52:19 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -129,22 +129,42 @@ public class App
         //     System.out.println("Grade: incredible!");
         // }
 
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Value of the gift?");
+        // int gift = Integer.valueOf(scanner.nextLine());
+        // if (gift >= 5000 && gift < 25000) {
+        //     System.out.println("Tax: " + (100 + (gift-5000)*0.08));
+        // } else if (gift >= 25000 && gift < 55000) {
+        //     System.out.println("Tax: " + (1700 + (gift-25000)*0.1));
+        // } else if (gift >= 55000 && gift < 200000) {
+        //     System.out.println("Tax: " + ((4700 + (gift-55000)*0.12)));
+        // } else if (gift >= 200000 && gift < 1000000) {
+        //     System.out.println("Tax: " + ((22100 + (gift-200000)*0.15)));
+        // } else if (gift >= 1000000) {
+        //     System.out.println("Tax: " + ((142100 + (gift-1000000)*0.17)));
+        // } else {
+        //     System.out.println("No tax!");
+        // }
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Value of the gift?");
-        int gift = Integer.valueOf(scanner.nextLine());
-        if (gift >= 5000 && gift < 25000) {
-            System.out.println("Tax: " + (100 + (gift-5000)*0.08));
-        } else if (gift >= 25000 && gift < 55000) {
-            System.out.println("Tax: " + (1700 + (gift-25000)*0.1));
-        } else if (gift >= 55000 && gift < 200000) {
-            System.out.println("Tax: " + ((4700 + (gift-55000)*0.12)));
-        } else if (gift >= 200000 && gift < 1000000) {
-            System.out.println("Tax: " + ((22100 + (gift-200000)*0.15)));
-        } else if (gift >= 1000000) {
-            System.out.println("Tax: " + ((142100 + (gift-1000000)*0.17)));
-        } else {
-            System.out.println("No tax!");
+        int numOfNum = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum = sum + number;
+            numOfNum = numOfNum + 1;
         }
+
+        System.out.println("Number of numbers: " + numOfNum);
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
