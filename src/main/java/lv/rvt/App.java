1558bca6b5db089975035d72b01a63948d31a5a0
23DP1DPale <166118206+23DP1DPale@users.noreply.github.com>
2024-10-08 10:12:27 +0000

    Advanced astrology

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -250,17 +250,17 @@ public class App
     //     }
     // }
     
-    public static void main(String[] args) {
-        printStars(5);
-        printStars(3);
-        printStars(9);
-        System.out.println();
-        printSquare(4);
-        System.out.println();
-        printRectangle(17, 3);
-        System.out.println();
-        printTriangle(4);
-    }
+    // public static void main(String[] args) {
+    //     printStars(5);
+    //     printStars(3);
+    //     printStars(9);
+    //     System.out.println();
+    //     printSquare(4);
+    //     System.out.println();
+    //     printRectangle(17, 3);
+    //     System.out.println();
+    //     printTriangle(4);
+    // }
 
     public static void printStars(int number) {
         for (int i = 0; i < number; i++) {
@@ -285,5 +285,37 @@ public class App
         for (int i = 0; i < size; i++) {
             printStars(i+1);
         }
+    }
+
+
+    public static void main(String[] args) {
+        printTriangle2(4);
+        christmasTree(10);
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printTriangle2(int size) {
+        for (int i = 0; i < size; i++) {
+            printSpaces(size-(i+1));
+            printStars(i+1);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        int stars = 1;
+        for (int i = 0; i < height; i++) {
+            printSpaces(height-(i+1));
+            printStars(stars);
+            stars += 2;
+        }
+        printSpaces(height-2);
+        printStars(3);
+        printSpaces(height-2);
+        printStars(3);
     }
 }
